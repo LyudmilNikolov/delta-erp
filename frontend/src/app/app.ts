80e45f94704b8b3d0bd9aa6e3a69319bc9a705be
Lyudmil Nikolov <nikolovlyudmill@gmail.com>
2025-06-18 21:13:10 +0300

    (FE): Update folder structure

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggle } from '@angular/material/slide-toggle';
+import {MatSlideToggle} from "@angular/material/slide-toggle";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MatSlideToggle],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrl: './app.scss'
 })
 export class App {
   protected title = 'delta-erp';
