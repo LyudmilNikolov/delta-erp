430712f2c046a5a64fd774e93cd1c4db6b49929c
Lyudmil Nikolov <nikolovlyudmill@gmail.com>
2025-06-18 21:40:16 +0300

    (FE): Update config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -14,22 +14,16 @@ module.exports = tseslint.config(
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
+      rules: {
+        "@angular-eslint/directive-selector": [
+          "error",
+          { type: "attribute", prefix: "app", style: "camelCase" }
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          { type: "element", prefix: "app", style: "kebab-case" }
+        ],
+      }
     },
   },
   {
