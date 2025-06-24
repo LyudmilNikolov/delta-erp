5e7e45e99cb44027a4eecceaf9541d03d35df968
Lyudmil Nikolov <nikolovlyudmill@gmail.com>
2025-06-24 21:35:22 +0300

    (FE): Add navbar

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatSlideToggle} from "@angular/material/slide-toggle";
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: ` <router-outlet /> `,
+  imports: [RouterOutlet, Navbar],
+  template: `
+    <app-navbar />
+    <router-outlet />
+  `,
 })
-export class App {
-}
+export class App {}
